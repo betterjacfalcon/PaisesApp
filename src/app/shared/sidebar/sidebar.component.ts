8f805887a1150923d1ea6b208ef570025dc2617f
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-13 20:11:52 -0600

    Separacion de busqueda para el componente Sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styles: [
+    `
+      li {
+        cursor:pointer
+      }
+    
+    `
+  ]
 })
 export class SidebarComponent implements OnInit {
 
