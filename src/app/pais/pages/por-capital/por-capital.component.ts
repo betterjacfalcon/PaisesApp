46c24d7c5bc94fc588c4ea9443f4938dd04c6f3e
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-07-28 06:01:33 -0600

    Se prgramo el componente de input, se programo la busqueda por Capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
   styleUrls: ['./por-capital.component.css']
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
 
-  constructor() { }
+  termino:string = '';
+  hayError:boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  //Injection del Servicios Pais => private paisService: PaisService 
+  constructor(private paisService: PaisService) { }
+  buscar(termino:string){
+    this.hayError = false;
+    this.termino = termino;
+    
+    //Para que un Observable se active se requiere un subscribe
+    this.paisService.buscarCapital(this.termino)
+    .subscribe( paises => {
+      console.log(paises);  
+      this.paises = paises;   
+    }, (err) =>{
+      this.hayError = true;
+      this.paises = [];
+    })
+  } 
+
+  sugerencia(termino:string){
+    this.hayError = false;
+    //TODO: crear sugerencias
   }
 
 }
