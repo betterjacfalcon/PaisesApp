026bf8ddfb0597645524ee4cf34eb5f378b28b97
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-08-09 06:26:23 -0600

    ver pais de forma independiente

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -7,20 +7,31 @@ import { Country } from '../interfaces/pais.interface';
   providedIn: 'root'
 })
 export class PaisService {
-  private apiURL:string = 'https://restcountries.com/v3.1';
+  private apiURL: string = 'https://restcountries.com/v3.1';
 
   //Injection del Servicios Http private http: HttpClient  
   constructor(private http: HttpClient) { }
 
-  buscarPais( termino: string):Observable<Country[]> {
-  
-      const url = `${this.apiURL}/name/${ termino}`;
-      return this.http.get<Country[]>(url);
+  buscarPais(termino: string): Observable<Country[]> {
+
+    const url = `${this.apiURL}/name/${termino}`;
+    return this.http.get<Country[]>(url);
   }
 
-  buscarCapital( termino: string):Observable<Country[]> {
-  
-    const url = `${this.apiURL}/capital/${ termino}`;
+  buscarCapital(termino: string): Observable<Country[]> {
+
+    const url = `${this.apiURL}/capital/${termino}`;
     return this.http.get<Country[]>(url);
-}
+  }
+  /*getPaisPorAlpa(id: string): Observable<Country[]> {
+
+    const url = `${this.apiURL}/capital/${termino}`;
+    return this.http.get<Country[]>(url);
+  }*/
+  getPaisPorAlpa(id: string): Observable<Country> {
+
+    const url = `${this.apiURL}/alpha/${id}`;
+    return this.http.get<Country>(url);
+  }
+
 }
