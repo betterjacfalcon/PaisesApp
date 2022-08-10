02aaf43f1fe431224a3755d796a0990f27503098
Alejandra Rodriguez <jacfalcon.better.practices@gmail.com>
2022-08-10 13:05:11 -0600

    Obtener datos de interfaz contry para mostrarlo por pantalla4

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap} from 'rxjs/operators'; //recibe un observable y regresa un observable
+import { switchMap, tap} from 'rxjs/operators'; //switchMap - recibe un observable y regresa un observable --tap - un operador que dispara un efecto secundario
 import { PaisService } from '../../services/pais.service';
+import { Country} from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -9,6 +10,8 @@ import { PaisService } from '../../services/pais.service';
   styleUrls: ['./ver-pais.component.css']
 })
 export class VerPaisComponent implements OnInit {
+
+  pais!: Country;
 
   constructor(
     private activedRote: ActivatedRoute,
@@ -18,20 +21,10 @@ export class VerPaisComponent implements OnInit {
   ngOnInit(): void {
     this.activedRote.params
         .pipe(
-         switchMap(({id }) => this.paisService.getPaisPorAlpa(id))
+         switchMap(({id }) => this.paisService.getPaisPorAlpa(id)),         
+         tap (console. log) //devuelve lo que se obtubo de switcMap
         )
-        .subscribe( resp => {
-          console.log( resp)
-        });
-    /*this.activedRote.params.
-      subscribe( ({id}) => {
-        console.log(id);
-
-        this.paisService.getPaisPorAlpa( id )
-        .subscribe( pais => {
-          console.log( pais);
-        })
-      })*/
+        .subscribe( pais =>   this.pais = pais[0]);    
   }
 
 }
